65ac86e6f373aa5eed13fa2dd5c6e1500941a844
2624789 <2624789@users.noreply.github.com>
2019-10-22 11:21:40 -0500

    Adds welcome sliders

--- screens/Welcome.js
+++ screens/Welcome.js
@@ -1,41 +1,76 @@
 import React from 'react';
 import {
-  SafeAreaView,
   StyleSheet,
-  ScrollView,
   View,
   Text,
-  StatusBar,
+  TouchableOpacity,
   Image,
-  TouchableOpacity
 } from 'react-native';
+import Swiper from 'react-native-swiper'
 
 import { colors } from '../envStyles';
 
 const Welcome = ({navigation}) => {
-
-
   return(
-    <View style={styles.container}>
-      <Text>
-        Welcome
-      </Text>
-      <TouchableOpacity
-        onPress={() => navigation.navigate('Register')}
-        style={styles.button}
-      >
-        <Text style={styles.buttonText}>Muévete</Text>
-      </TouchableOpacity>
-    </View>
+    <Swiper
+      style={styles.wrapper}
+      showsButtons={false}
+      loop={false}
+      dot={<View style={{backgroundColor: 'rgba(255,255,255,.3)', width: 8, height: 8, borderRadius: 4, marginLeft: 3, marginRight: 3}} />}
+      activeDot={<View style={{backgroundColor: '#fff', width: 8, height: 8, borderRadius: 4, marginLeft: 3, marginRight: 3}} />}>
+      <View style={styles.slide}>
+        <Image
+          source={require('../assets/images/01_Bienvenida.png')}
+          style={styles.stretch}
+        />
+        <Text style={styles.text}>aquí encontrarás servicios y productos para lo que te mueve</Text>
+      </View>
+      <View style={styles.slide}>
+        <Image
+          source={require('../assets/images/02_Quetemueve.png')}
+          style={styles.stretch}
+        />
+        <Text style={styles.text}>carro, moto, bici, scooter, caminar...</Text>
+      </View>
+      <View style={styles.slide}>
+        <Image
+          source={require('../assets/images/03_Elige.png')}
+          style={styles.stretch}
+        />
+        <Text style={styles.text}>tu producto o servicio y paga por la aplicación</Text>
+      </View>
+      <View style={styles.slide}>
+        <Image
+          source={require('../assets/images/04_Ahora.png')}
+          style={styles.stretch}
+        />
+        <Text style={styles.text}>que lo sabes</Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Register')}
+          style={styles.button}
+        >
+          <Text style={styles.buttonText}>Muévete</Text>
+        </TouchableOpacity>
+      </View>
+    </Swiper>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  wrapper: {},
+  slide: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: colors.purple,
+    padding: 20
+  },
+  text: {
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 18,
+    color: colors.white,
+    textAlign: 'center',
+    marginBottom: 20,
   },
   button: {
     borderColor: colors.yellow,
@@ -61,7 +96,11 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontFamily: 'Montserrat-SemiBold',
     color: 'black'
-  }
+  },
+  stretch: {
+    height: 400,
+    resizeMode: 'contain',
+  },
 });
 
 export default Welcome;
