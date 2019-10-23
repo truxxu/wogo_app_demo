6aec4b6d10862c5c960eaebe062a34bb8846816a
2624789 <2624789@users.noreply.github.com>
2019-10-23 18:55:07 -0500

    Fixes merge conflicts

--- keys.js
+++ keys.js
@@ -1,9 +1,5 @@
 export const env = {
-<<<<<<< HEAD
   apiServer: 'http://192.168.0.21:8000'
-=======
-  apiServer: 'http://192.168.0.15:8000'
->>>>>>> b7ba1bd62560338851e604cecc16729f6cdc3d00
 };
 
 export const geocoding = {
